8f84641b19bb208db3698b3a8790e1cb539a6012
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2021-01-29 14:14:43 +0100

    add date

---
+++ src/data.js
@@ -0,0 +1,41 @@
+const people = [
+  {
+    id: 1,
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg',
+    name: 'paula kowalsky',
+    title: 'office manager',
+    quote:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. In quam laboriosam quasi exercitationem pariatur quibusdam vero beatae harum! Totam exercitationem illo maxime ducimus recusandae vitae repellat modi consectetur odio asperiores!',
+  },
+  {
+    id: 2,
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
+    name: 'john bideners',
+    title: 'regular guy',
+    quote:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. In quam laboriosam quasi exercitationem pariatur quibusdam vero beatae harum! Totam exercitationem illo maxime ducimus recusandae vitae repellat modi consectetur odio asperiores!',
+  },
+  {
+    id: 3,
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959121/person-1_aufeoq.jpg',
+    name: 'peter smith',
+    title: 'product designer',
+    quote:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. In quam laboriosam quasi exercitationem pariatur quibusdam vero beatae harum! Totam exercitationem illo maxime ducimus recusandae vitae repellat modi consectetur odio asperiores!',
+  },
+  {
+    id: 4,
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
+    name: 'susan andersen',
+    title: 'the boss',
+    quote:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. In quam laboriosam quasi exercitationem pariatur quibusdam vero beatae harum! Totam exercitationem illo maxime ducimus recusandae vitae repellat modi consectetur odio asperiores!',
+  },
+];
+
+export default people;
+
