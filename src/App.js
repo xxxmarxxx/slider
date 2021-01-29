85472cf6d4796a4ffec1b7d37f2402e33cbfeb3d
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2021-01-29 14:31:03 +0100

    add const

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import React, { useState, useEffect } from 'react';
-import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
-import { FaQuoteRight } from 'react-icons/fa';
-import data from './data';
+import React, { useState, useEffect } from "react";
+import { FiChevronRight, FiChevronLeft } from "react-icons/fi";
+import { FaQuoteRight } from "react-icons/fa";
+import data from "./data";
 function App() {
-  return <h2>slider project setup</h2>;
+  const [poeple, setPeople] = useState(data);
+  const [index, setIndex] = useState(0)
+  return <section className="section">
+    <div className="title">
+      <h2>
+        <span>/</span>reviews
+      </h2>
+    </div>
+    <div className="section-center">
+  {poeple.map((person,personIndex)=>{
+    const {id,image,name,title,quote} = person;
+    // more stuff coming up
+  })}
+    </div>
+  </section>;
 }
 
 export default App;
